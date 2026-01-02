f2b95d3f0a8870d4a864fb045ff5fa0267e89841
Chanwu Gim <1stpkloser@naver.com>
2026-01-02 18:00:08 +0900

    GAS 수정

--- js/manual.js
+++ js/manual.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     // ===================================================================
     // ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼ 설정 영역 ▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼▼
     // ===================================================================
-    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbzo2pWiRjXt25jvQmc-72Dudiccil09EGNVd-_eW3efSgWiFuwk8eGDljWQNkZ3dafALQ/exec';
+    const GAS_WEB_APP_URL = 'https://script.google.com/macros/s/AKfycbxbXFFOceFBRJDvKfKlRF1RRrQ2nbm_Xj2epeLcuvhPL9LqzbZffXCCMK1TcBZU7JD6/exec';
     const API_KEY = 'GEM-PROJECT-GPH-2025';
     // ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲ 설정 영역 ▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲▲
     // ===================================================================
